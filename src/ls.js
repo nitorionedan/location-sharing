0e72a258275477894e649e199a997afa0edb850b
nitorionedan <fearinatunitori@gmail.com>
2023-05-08 17:34:24 +0900

    dev(#3): displays a location embedded in URL

--- src/ls.js
+++ src/ls.js
@@ -24,28 +24,59 @@ const popupMsg = () => {
     return `現在地<br>${hours}:${minutes}`
 };
 
-// モバイルユーザーかどうか
-const isMobileUser = () => navigator.userAgent.match(/iPhone|Android.+Mobile/);
-
 // メイン処理
 window.onload = () => {
-    // URL経度緯度パラメータを解析
-    let url = location.href;
-    let paramLatLng = url.substring(url.indexOf("#") + 1, url.length);
-    let lat = parseFloat(paramLatLng.split("/")[0]);
-    let lng = parseFloat(paramLatLng.split("/")[1]);
-    if (!isNaN(lat) && !isNaN(lng)) {
-        paramLatLng = [lat, lng];
-    }
-    else {
-        paramLatLng = undefined;
-    }
-
     // Leafletマップオブジェクトを作成
     map = L.map("map", {doubleClickZoom: false});
     
-    // 現在地を地図で表示
-    map.locate({setView: true, maxZoom: defaultZoomLvl});
+    // URL経度緯度パラメータを解析
+    const url = location.href;
+    const paramLatLng = url.includes("#") ? url.substring(url.indexOf("#") + 1, url.length) : "";
+    const paramLat = parseFloat(paramLatLng.split("/")[0]);
+    const paramLng = parseFloat(paramLatLng.split("/")[1]);
+
+    // 位置情報パラメータがあればその位置を、なければ現在地を表示する
+    if (isNaN(paramLat) || isNaN(paramLng)) {
+        console.log("no param");
+
+        // 現在地を地図で表示
+        map.locate({setView: true, maxZoom: defaultZoomLvl});
+
+        // 現在地にピン
+        map.once("locationfound", (e) => {
+            if (!marker) {
+                marker = L.marker(e.latlng).addTo(map);
+            } else {
+                marker.setLatLng(e.latlng).addTo(map);
+            }
+
+            // 現在地情報を控える
+            currentLatLng = {lat: e.latlng.lat, lng: e.latlng.lng};
+
+            // ピンに現在時刻を表示
+            marker.bindPopup(popupMsg()).openPopup();
+        });
+    } else {
+        console.log(`lat: ${paramLat}, lng: ${paramLng}`);
+
+        // 現在地にピン
+        const latlng = [paramLat, paramLng];
+        map.setView(latlng, 11, { animation: true });
+        map.locate()
+        map.once("locationfound", () => {
+            if (!marker) {
+                marker = L.marker(latlng).addTo(map);
+            } else {
+                marker.setLatLng(latlng).addTo(map);
+            }
+
+            // 現在地情報を控える
+            currentLatLng = {lat: paramLat, lng: paramLng};
+
+            // ピンに現在時刻を表示
+            marker.bindPopup(popupMsg()).openPopup();
+        });
+    }
 
     // タイルレイヤーを設定
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -54,30 +85,18 @@ window.onload = () => {
     }).addTo(map);
     L.Control.geocoder().addTo(map);
 
-    // 現在地にピン
-    map.once("locationfound", (e) => {
-        if (!marker) {
-            marker = L.marker(e.latlng).addTo(map);
-        } else {
-            marker.setLatLng(e.latlng).addTo(map);
-        }
-
-        // 現在地情報を控える
-        currentLatLng = {lat: e.latlng.lat, lng: e.latlng.lng};
-
-        // ピンに現在時刻を表示
-        marker.bindPopup(popupMsg()).openPopup();
-    });
-
+    // モバイルなら定期的に位置を更新する
     if (!navigator.geolocation) {
         console.log("Error: Geolocation機能がありません。");
     } else {
-        if (isMobileUser()) {
+        if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
             setInterval(() => {
                 navigator.geolocation.getCurrentPosition(getPosition);
             }, 5000);
         }
     }
+
+    initShareButton();
 };
 
 // お試し用
@@ -95,7 +114,8 @@ function quickStart(map) {
         currentLatLng = {lat: e.latlng.lat, lng: e.latlng.lng};
 
         // ピンに現在時刻を表示
-        marker.bindPopup(`現在地<br>${hours()}:${minutes()}`).openPopup();
+        const today = new Date();
+        marker.bindPopup(`現在地<br>${today.getHours()}:${today.getMinutes()}`).openPopup();
     });
 
     // debug
@@ -108,10 +128,12 @@ function quickStart(map) {
 function initShareButton() {
     const btn = document.getElementById("share-button");
     btn.addEventListener("click", async () => {
+        const today = new Date();
         const shareData = {
             title: "現在地を共有",
-            text: `経度緯度(${currentLatLng.lat}, ${currentLatLng.lng}), 時刻(${hours()}:${minutes()})`,
-            url: `https://rainyseason-rainy-days.netlify.app/ls.html#${currentLatLng.lat}/${currentLatLng.lng}`
+            text: `経度緯度(${currentLatLng.lat}, ${currentLatLng.lng}), 時刻(${today.getHours()}:${today.getMinutes()})`,
+            // url: `https://rainyseason-rainy-days.netlify.app/ls.html#${currentLatLng.lat}/${currentLatLng.lng}`
+            url: `#${currentLatLng.lat}/${currentLatLng.lng}`
         };
     
         try {
