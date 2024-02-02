f4da2cdf8beac094dd160dd082cf96d7916e6633
nitorionedan <fearinatunitori@gmail.com>
2024-02-02 18:08:09 +0900

    dev: remove env.js from .gitignore

---
+++ env.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbFgb5ISOnsa92ubMkSXwHmu0cEYL9NBs",
+  authDomain: "fir-tutorial-40a1d.firebaseapp.com",
+  projectId: "fir-tutorial-40a1d",
+  storageBucket: "fir-tutorial-40a1d.appspot.com",
+  messagingSenderId: "559146471206",
+  appId: "1:559146471206:web:eff4998c41d66b774cbbe7",
+  measurementId: "G-W7DHVK8FKP"
+};
+  
